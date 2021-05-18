704444b735eca9de51e0d558beb6280b164461ef
Joel Shine <70741696+JoelShine@users.noreply.github.com>
2021-05-18 17:50:48 +0530

    Added class file also

---
+++ Current_date_and_time.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Current_date_and_time {
+    public static void main(String[] args) {
+        Date now = new Date();
+        
+        System.out.println("Hello User !");
+        System.out.println("Current time = " + now);
+    }
+}
